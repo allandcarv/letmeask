4e1b9a519432499ccf39d249814aa5df6c10c4ce
Allan Carvalho <allandcarv@gmail.com>
2021-06-23 19:53:23 +0100

    First Class

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/Button';
+
+import './services/firebase';
+
+function App() {
+  return (
+  <>
+    <h1>Hello World</h1>
+    <Button></Button>
+  </>
+  );
+}
+
+export default App;
